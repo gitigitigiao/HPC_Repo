866ecff07831029077c838cc629d3776542b4db5
ambarha <237358056@qq.com>
2026-02-13 21:06:14 +0800

    openmp

---
+++ OpenMP.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<int> numbers = {1, 2, 3, 4, 5};
+
+    // 用途: 将循环的迭代分配给多个线程并行执行。
+    #pragma omp parallel
+    {
+        std::cout << "Thread " << omp_get_thread_num() << " is processing the vector." << std::endl;
+    }
+
+    // 用途：指定代码块只由第一个到达线程执行，其他线程跳过该代码块。
+    #pragma omp parallel for
+    for (size_t i = 0; i < numbers.size(); ++i) {
+        numbers[i] *= 2; // Doubling each number in the vector
+    }
+    std::cout << "Numbers in the vector: ";
+    for (const auto& num : numbers) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
